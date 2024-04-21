e4f270cc94619ad402cd9d1846dea4edd976cdf5
Khondhaker Al Momin <mominbuet41@gmail.com>
2024-04-21 09:19:10 -0500

    Updates

--- forms/contact.php
+++ forms/contact.php
@@ -1,36 +1,31 @@
 <?php
- 
-
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'momin.ce12@gmail.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require 'assets/vendor/php-email-form/PHPMailer/src/Exception.php';
+require 'assets/vendor/php-email-form/PHPMailer/src/PHPMailer.php';
+require 'assets/vendor/php-email-form/PHPMailer/src/SMTP.php';
+$mail = new PHPMailer(true);
+try {
+    // Server settings
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'momin.ce12@gmail.com';        // SMTP username
+    $mail->Password   = 'your-gmail-app-password';              // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+    // Recipients
+    $mail->setFrom('your-gmail-username@gmail.com', 'Mailer');
+    $mail->addAddress('momin.ce12@gmail.com', 'Khondhaker Al Momin');     // Add a recipient
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $_POST['subject'];
+    $mail->Body    = 'This is a plain-text message body';
+    $mail->AltBody = strip_tags($_POST['message']);
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 ?>
